435c16cc212398d7182883a2b29d2592b9d930b7
Lester Caine <lester@lsces.co.uk>
2005-08-11 16:25:32 +0000

    Tidy up BitSiticky to LibertyContent and expand comments

--- BitSticky.php
+++ BitSticky.php
@@ -16,20 +16,19 @@
 // | Authors: spider <spider@steelsun.com>
 // +----------------------------------------------------------------------+
 //
-// $Id: BitSticky.php,v 1.1.1.1.2.4 2005/08/07 16:25:54 lsces Exp $
+// $Id: BitSticky.php,v 1.1.1.1.2.5 2005/08/11 16:25:32 lsces Exp $
 
 /**
  * required setup
  */
-require_once( LIBERTY_PKG_PATH.'LibertyAttachable.php' );
+require_once( LIBERTY_PKG_PATH.'LibertyContent.php' );
 
 define('TIKISTICKY_CONTENT_TYPE_GUID', 'tikisticky' );
 
 /**
  * @package	stickies
- * @subpackage BitSticky
  */
-class BitSticky extends LibertyAttachable {
+class BitSticky extends LibertyContent {
 
 	function BitSticky( $pStickyId=NULL, $pContentId=NULL, $pNotatedContentId=NULL ) {
 		LibertyAttachable::LibertyAttachable();
@@ -48,6 +47,16 @@ class BitSticky extends LibertyAttachable {
 	}
 
 
+	/**
+	 * Load a sticky object identified by mStickyId, $this->mNotatedContentId or mContentId in that order
+	 *
+	 * Populates the mInfo array with the following fields
+	 *
+	 * sticky_id
+	 * notated_content_id
+	 * parsed
+	 * @return integer count of number of fields in MInfo
+	 */
 	function load() {
 		if( !empty( $this->mStickyId ) || !empty( $this->mContentId )  || !empty( $this->mNotatedContentId ) ) {
 			if( !empty( $this->mStickyId ) ) {
@@ -72,12 +81,28 @@ class BitSticky extends LibertyAttachable {
 				$this->mInfo['parsed'] = $this->parseData();
 				$this->mContentId = $result->fields['content_id'];
 				$this->mStickyId = $result->fields['sticky_id'];
-				LibertyAttachable::load();
+				LibertyContent::load();
 			}
 		}
 		return( count( $this->mInfo ) );
 	}
 	
+    /**
+     * Verify the core class data required to update the tiki_content table entries
+	 *
+	 * @param array Array of content data to be stored 
+	 * pParamHash Array 
+	 * (See LibertyContent::verify for details of the core fields - which
+	 * appends a [content_store] array with all the values for tiki_content)
+	 *
+	 * content_type_guid	string	Should contain 'tikisticky'
+	 * notated_content_id	integer	content_id of the object to which the stickie isattached
+	 * sticky_store - Array of values for entering in tiki_stickies
+	 *		sticky_id			integer		If existing then current sticky id
+	 *										otherwise populate from sequence
+	 *		content_id			integer		Content id of the note
+	 *		notated_content_id	integer		Content id of the object to which it is attached
+	 */
 	function verify( &$pParamHash ) {
 		// It is possible a derived class set this to something different
 		if( empty( $pParamHash['content_type_guid'] ) ) {
@@ -97,38 +122,49 @@ class BitSticky extends LibertyAttachable {
 		return( count( $this->mErrors ) == 0 );
 	}	
 	
+    /**
+     * Create a new stickies object or update an existing one
+	 *
+	 * @param array Array of content data to be stored (see verify for details)
+	 * @return integer Number of errors detected ( 0 if successful )
+	 */
 	function store( &$pParamHash ) {
-		$this->mDb->StartTrans();
-		if( $this->verify( $pParamHash ) && LibertyAttachable::store( $pParamHash ) ) {
-            if( $this->mStickyId ) {
-				$stickyId = array ( "name" => "sticky_id", "value" => $this->mStickyId );
-				$result = $this->mDb->associateUpdate( BIT_DB_PREFIX."tiki_stickies", $pParamHash['sticky_store'], $stickyId );
-			} else {
-				$pParamHash['sticky_store']['content_id'] = $pParamHash['content_id'];
-				if( isset( $pParamHash['page_id'] ) && is_numeric( $pParamHash['sticky_id'] ) ) {
-					$pParamHash['sticky_store']['sticky_id'] = $pParamHash['sticky_id'];
+		if( $this->verify( $pParamHash ) ) {
+			$this->mDb->StartTrans();
+			if( LibertyContent::store( $pParamHash ) ) {
+            	if( $this->mStickyId ) {
+					$stickyId = array ( "name" => "sticky_id", "value" => $this->mStickyId );
+					$result = $this->mDb->associateUpdate( BIT_DB_PREFIX."tiki_stickies", $pParamHash['sticky_store'], $stickyId );
 				} else {
-					$pParamHash['sticky_store']['sticky_id'] = $this->mDb->GenID( 'tiki_stickies_sticky_id_seq');
-				}
-				$this->mPageId = $pParamHash['sticky_store']['sticky_id'];
+					$pParamHash['sticky_store']['content_id'] = $pParamHash['content_id'];
+					if( isset( $pParamHash['page_id'] ) && is_numeric( $pParamHash['sticky_id'] ) ) {
+						$pParamHash['sticky_store']['sticky_id'] = $pParamHash['sticky_id'];
+					} else {
+						$pParamHash['sticky_store']['sticky_id'] = $this->mDb->GenID( 'tiki_stickies_sticky_id_seq');
+					}
+					$this->mPageId = $pParamHash['sticky_store']['sticky_id'];
 
-				$result = $this->mDb->associateInsert( BIT_DB_PREFIX."tiki_stickies", $pParamHash['sticky_store'] );
+					$result = $this->mDb->associateInsert( BIT_DB_PREFIX."tiki_stickies", $pParamHash['sticky_store'] );
+				}
+				$this->mDb->CompleteTrans();
+			} else {
+				$this->mDb->RollbackTrans();
 			}
-			$this->mDb->CompleteTrans();
-		} else {
-			$this->mDb->RollbackTrans();
 		}
 
 		return( count( $this->mErrors ) == 0 );
 	}
 	
+	/**
+	 * Delete stickies object and related content record
+	 */
 	function expunge() {
 		$ret = FALSE;
 		if( $this->isValid() ) {
 			$this->mDb->StartTrans();
 			$query = "DELETE FROM `".BIT_DB_PREFIX."tiki_stickies` WHERE `content_id` = ?";
 			$result = $this->mDb->query( $query, array( $this->mContentId ) );
-			if( LibertyAttachable::expunge() ) {
+			if( LibertyContent::expunge() ) {
 				$ret = TRUE;
 				$this->mDb->CompleteTrans();
 			} else {
