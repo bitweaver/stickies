059f679dd71763042d5a821750a0d206f81373cb
Max Kremmel <xing@synapse.plus.com>
2006-05-06 15:48:11 +0000

    clean up schema files, remove unused PackageInfo data - we really need a package manager for this, add plugin_path column to liberty_plugins

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -22,9 +22,6 @@ foreach( array_keys( $tables ) AS $tableName ) {
 $gBitInstaller->registerPackageInfo( STICKIES_PKG_NAME, array(
 	'description' => "A personal sticky system for adding stickies to any content.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '0.1',
-	'state' => 'beta',
-	'dependencies' => '',
 ) );
 
 // ### Indexes
